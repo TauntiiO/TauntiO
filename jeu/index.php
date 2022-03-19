91bddf03b51bec5d4cdcda1834f0dc16735af7a7
TauntiO <titouan.br64@gmail.com>
2022-03-19 16:06:02 +0100

    OMG ça marche

---
+++ jeu/index.php
@@ -0,0 +1,37 @@
+<?php
+    $dossier='../sounds';
+
+    $list = opendir($dossier);
+  
+    $tabfile = array();
+    
+    while ($fichier = readdir($list)) 
+    {
+    ($fichier != "." && $fichier != ".." && $fichier != ".htaccess")? $tabfile[] = $fichier : '' ;
+    }
+    closedir($list);
+  
+  //mélange du tableau
+    shuffle($tabfile);
+  
+  // premier élément du tableau
+  $premier = array_shift($tabfile);
+  $voiceline = $dossier.'/'.$premier;
+?>
+
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>TauntiO</title>
+</head>
+<body>
+    <button class="button" onclick="window.location.href = '../index.html';"><span>Accueil</span></button>
+    <audio controls src=$voiceline>Audio aléatoire</audio>
+    <?php echo $voiceline ?>
+</body>
+</body>
+</html>
